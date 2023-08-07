c594449c0bc02dbb9fc87657c8776a005e29d965
buddhasystem <potekhin@bnl.gov>
2023-08-07 11:17:25 -0400

    Cleanup.

--- bms-test.py
+++ bms-test.py
@@ -11,17 +11,30 @@ from bms.parts import *
 battery = Battery(11.6)
 print('Battery voltage:', battery.voltage)
 
-controller = Controller(battery)
-print('Battery voltage from controller:', controller.battery.voltage)
+ctr = Controller(battery)
+print('Battery voltage from controller:', ctr.battery.voltage)
 
 battery.set_voltage(10.1)
-print('Battery voltage from controller:', controller.battery.voltage)
+print('Battery voltage from controller:', ctr.battery.voltage)
 
-o = O("2025-02-04 00:00:00 to 2025-02-05 23:45:00") # 2025-03-07 23:45:00
-print(o.times)
 
-(alt, az) = o.get_track_solar('sun')
 
-print(alt)
+ctr.add_panel(WPanel('W'))
+ctr.add_panel(EPanel('E'))
+ctr.add_panel(TPanel('T'))
+
+for p in ctr.panels:
+    print(f'''{p.name}''')
+    print(p.normal_rot)
+
+
+
+
+
+
+#o = O("2025-02-04 00:00:00 to 2025-02-05 23:45:00") # 2025-03-07 23:45:00
+#print(o.times)
+#(alt, az) = o.get_track_solar('sun')
+#print(alt)
 
 
--- bms/parts.py
+++ bms/parts.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from    scipy.spatial.transform import Rotation as R
 
 class Battery:
     def __init__(self, voltage=0.0):
@@ -15,8 +16,10 @@ class Controller:
         self.devices    = []
         self.battery    = battery
 
-#    def set_voltage(self, voltage):
-#        self.voltage = voltage
+    def add_panel(self, panel):
+        self.panels.append(panel)
+
+    
 
 class Device:
     def __init__(self, name='', voltage=0.0):
@@ -29,23 +32,35 @@ class Device:
 ###
 
 class Panel:
-    # Assume that the pivot angle is zero for now, easy to add later:
+    ### Assume that the pivot angle is zero for now, easy to add later:
     name =''
+    lander_pitch, lander_roll, lander_yaw = 0., 0., 0.
+
+    ### Define lander rotations
+    r1 = R.from_euler('x', lander_pitch,    degrees=True) # + is nose down,     - is nose up
+    r2 = R.from_euler('y', lander_roll,     degrees=True) # + is top left,      - is top right
+    r3 = R.from_euler('z', lander_yaw,      degrees=True) # + is nose right,    - is nose left
+    r_tot = r1*r2*r3
+
     def __init__(self, name=''):
         self.name = name
         self.normal = (None, None, None)
+        self.normal_rot = (None, None, None)
 
 class EPanel(Panel):
     def __init__(self, name=''):
         Panel.__init__(self, name)
         self.normal = (1., 0., 0.)
+        self.normal_rot = self.r_tot.apply(self.normal)
 
 class WPanel(Panel):
     def __init__(self, name=''):
         Panel.__init__(self, name)
         self.normal = (-1., 0., 0.)
+        self.normal_rot = self.r_tot.apply(self.normal)
 
 class TPanel(Panel):
     def __init__(self, name=''):
         Panel.__init__(self, name)
         self.normal = (0., 0., 1.)
+        self.normal_rot = self.r_tot.apply(self.normal)
