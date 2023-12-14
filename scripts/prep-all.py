3b86f3a2fcf8cf78b07e6f637e360b7067855c5f
buddhasystem <potekhin@bnl.gov>
2023-12-14 16:36:08 -0500

    Added an updated stub for the "prep-all" script, and stubs for the modes and command table YAML

--- scripts/prep-all.py
+++ scripts/prep-all.py
@@ -1,62 +1,53 @@
 #! /usr/bin/env python
+#######################################################################
+# The script to prepare ALL of the conditions data e.g. Sun, satellites
+#######################################################################
+
 import argparse
+import yaml
 
 from nav.coordinates    import *
 
-# The script to prepare ALL of the conditions data. NB. on hold due to refactoring
+# Pretty print the dictionary we read from the input YAML, for an extra check:
+def pretty(d, indent=0):
+    for key, value in d.items():
+        print('\t' * indent + str(key))
+        if isinstance(value, dict):
+            pretty(value, indent+1)
+        else:
+            print('\t' * (indent+1) + str(value))
+
 
 #######################################
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-v", "--verbose",      action='store_true', help="Verbose mode")
-parser.add_argument("-V", "--veryverbose",  action='store_true', help="Very verbose mode")
-
-parser.add_argument("-c", "--cachefile",    type=str,            help="The cache file (output)", default='')
-parser.add_argument("-t", "--timerange",    type=str,            help="The time range", default='')
-
-parser.add_argument("-b", "--begin",        type=str,            help="Begin (start)", default='')
-parser.add_argument("-e", "--end",          type=str,            help="End (stop)", default='')
+parser.add_argument("-y", "--yamlfile",     type=str,            help="The input - a YAML file containing configuration", default='')
 #######################################
 
 # Example of the time range: "2025-02-04 00:00:00 to 2025-03-07 23:45:00"
 
 args    = parser.parse_args()
 
-cachefile   = args.cachefile
-timerange   = args.timerange
-
+yamlfile    = args.yamlfile
 verb        = args.verbose
-very        = args.veryverbose
 
-begin       = args.begin
-end         = args.end
 
 # ---
 if verb:
     print("*** Verbose mode ***")
-    print(f'''*** Cache file (output): "{cachefile}", begin: "{begin}, end: {end}" ***''')
+    print(f'''*** Configuration file (YAML): "{yamlfile}" ***''')
 
 
-if begin=='' or end=='':
-    if verb:
-        print('Missing inputs, exiting...')
-    exit(-1)
+f = open(yamlfile, 'r')
 
-(times, alt, az) = track((begin, end)) # "track" is imported from "coordinates", and wraps "Observation"
+content = yaml.safe_load(f)
 
-N = times.size
 
-if verb: print(f'''Number of points: {N}''')
+if verb:
+    print("*** Top-level keys ***")
+    print(*content.keys())
+    print("*** Content ***")
+    pretty(content)
 
-mjds = np.empty(N)
-for i in range(N): mjds[i] = times[i].mjd # FIXME - change to list comprehension later
-
-result = np.column_stack((mjds, alt, az))
-
-if very: print(result)
-
-if cachefile != '':
-    with open(cachefile, 'wb') as f:
-        np.save(f, result)
-    
 exit(0)
