9e6d853fc46052aa799af0b4f77983ae23504b9e
buddhasystem <potekhin@bnl.gov>
2023-12-14 22:22:28 -0500

    Added to the test YAML files, converged on metadata storage in HDF5

--- scripts/prep-all.py
+++ scripts/prep-all.py
@@ -6,6 +6,8 @@
 import argparse
 import yaml
 
+import h5py
+
 # Pretty print the dictionary we read from the input YAML, for an extra check:
 def pretty(d, indent=0):
     for key, value in d.items():
@@ -21,6 +23,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-v", "--verbose",      action='store_true', help="Verbose mode")
 parser.add_argument("-y", "--yamlfile",     type=str,            help="The input - a YAML file containing configuration", default='')
+parser.add_argument("-o", "--outputfile",   type=str,            help="The outpuut", default='')
 #######################################
 
 # Example of the time range: "2025-02-04 00:00:00 to 2025-03-07 23:45:00"
@@ -28,6 +31,7 @@ parser.add_argument("-y", "--yamlfile",     type=str,            help="The input
 args    = parser.parse_args()
 
 yamlfile    = args.yamlfile
+outputfile  = args.outputfile
 verb        = args.verbose
 
 
@@ -35,6 +39,7 @@ verb        = args.verbose
 if verb:
     print("*** Verbose mode ***")
     print(f'''*** Configuration file (YAML): "{yamlfile}" ***''')
+    print(f'''*** Output file (YAML): "{outputfile}" ***''')
 
 
 f = open(yamlfile, 'r')
@@ -47,5 +52,48 @@ if verb:
     print(*content.keys())
     print("*** Content ***")
     pretty(content)
+
+
+f = h5py.File(outputfile, 'w')
+
+
+groups = {}
+
+
+parsed = yaml.dump(content)
+
+
+dt = h5py.string_dtype(encoding='utf-8')
+
+
+grp = f.create_group('test')
+                     
+ds = grp.create_dataset('VLDS', (1,), dtype=dt)
+ds[0,] = parsed
+
+anew = yaml.safe_load(ds[0,])
+again = yaml.dump(anew)
+
+print(again)
+
+
+# for cntK, cntV in content.items():
+#     grp = f.create_group(cntK)
+
+#     if isinstance(cntV, dict):
+#         l = len(cntV)
+#         print('!', l, cntV)
+#         ds = grp.create_dataset('VLDS', (l,), dtype=dt)
+#         for i in range(0,l):
+#             ds[i] = cntV[i]
+#     groups[cntK] = grp
+
+
+#dt = h5py.string_dtype(encoding='utf-8')
+#ds = grp.create_dataset('VLDS', (100,100), dtype=dt)
+#ds[0,2] = 'foo'
+
+
+f.close()
 
 exit(0)
