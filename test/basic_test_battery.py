39a9684f0347af8ee51adc5bb089d15925c361ca
buddhasystem <potekhin@bnl.gov>
2024-03-16 17:18:28 -0400

    Small update in the battery test script

--- test/basic_test_battery.py
+++ test/basic_test_battery.py
@@ -4,6 +4,7 @@
 #######################################################################
 
 import yaml
+import argparse
 from   hardware        import *
 
 profile_yaml="""
@@ -19,8 +20,27 @@ VOC_table: ../data/hardware/battery/battery_VOC.dat
 VOC_table_cols: SOC VOC@0 R@0 VOC@20 R@20 VOC@40 R@40
 """
 
-print("Testing the battery", profile_yaml)
+##############################################
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-v", "--verbose", action='store_true', help="Verbose mode")
+parser.add_argument("-V", "--vocfile", type=str,            help="Location of the battery VOC data file", default='')
+args    = parser.parse_args()
+
+verbose = args.verbose
+vocfile = args.vocfile
+
 
 battery_config = yaml.safe_load(profile_yaml)
-print(battery_config)
-Battery(battery_config)
+
+if vocfile!='':
+    battery_config["VOC_table"]=vocfile
+
+if verbose:
+    print("Testing the battery", profile_yaml)
+    print(f'''Battery VOC data read from the file: "{battery_config["VOC_table"]}"''')
+
+# print(battery_config)
+battery = Battery(battery_config)
+
+if verbose: print(f'''Created a Battery with initial charge: {battery.level}, capacity: {battery.capacity}''')
