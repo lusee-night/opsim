1a9568180ef64c54c37eabb161ef86085eb175bc
buddhasystem <potekhin@bnl.gov>
2024-03-19 15:43:12 -0400

    Updated the README, to capture information from Anze

--- README.md
+++ README.md
@@ -6,8 +6,19 @@ generation and management, data taking (science), comms etc.
 
 __Glossary__
 
-* _modes_: reference to the modes of the LuSEE-Night apparatus' operations, such as "science"; this choice is in order to conform with the convention in the "ConOps" document.
+* _modes_: reference to the modes of the LuSEE-Night apparatus' operations, this choice is in order to conform with the convention in the "ConOps" document
+  * science
+  * main
+  * powersave
 * _states_: states of the devices as stipulated by the specific LuSEE _mode_.
+
+__Devices Included in the simulation__
+
+* _UT_ -- Comm module
+* _PDU_ -- Power distribution
+* _PCDU_ -- The battery management circuit
+* _PFPS_ -- PFPS picket fance power supply
+* _DCB_ -- Flight computer
 
 
 ## Folders in this repository
--- test/basic_test_battery.py
+++ test/basic_test_battery.py
@@ -7,6 +7,16 @@ import yaml
 import argparse
 from   hardware        import *
 
+
+# ---------------------------------------------------------
+import  lusee        # Core lusee software
+from    nav import * # Astro/observation wrapper classes
+from    utils.timeconv import *
+
+
+import  sim # Main simulation module, which contains the Simulator class
+from    sim import Simulator
+
 profile_yaml="""
 initial: 120.0 #in Ah
 capacity: 240.99 #in Ah
@@ -20,6 +30,13 @@ VOC_table: ../data/hardware/battery/battery_VOC.dat
 VOC_table_cols: SOC VOC@0 R@0 VOC@20 R@20 VOC@40 R@40
 """
 
+
+# Define paths in one place, can overwrite later
+orbitals    = "../data/orbitals/20260110-20270115.hdf5"
+modes       = "../config/modes.yml"
+devices     = "../config/devices.yml"
+comtable    = "../config/comtable-20260110-20270115.yml"
+
 ##############################################
 parser = argparse.ArgumentParser()
 
@@ -31,16 +48,35 @@ verbose = args.verbose
 vocfile = args.vocfile
 
 
-battery_config = yaml.safe_load(profile_yaml)
+initial_time=2
+until=4600
+#2780
 
-if vocfile!='':
-    battery_config["VOC_table"]=vocfile
+smltr = Simulator(orbitals, modes, devices, comtable, initial_time=initial_time, until=until, verbose=verbose)
+
+mjd_start   = smltr.sun.mjd[initial_time]
+mjd_end     = smltr.sun.mjd[until]
+
 
 if verbose:
-    print("Testing the battery", profile_yaml)
-    print(f'''Battery VOC data read from the file: "{battery_config["VOC_table"]}"''')
+    print(f'''Initial time in ticks: {initial_time}, mjd: {mjd_start}, datetime: {mjd2dt(mjd_start)}, Sun Alt: {smltr.sun.alt[initial_time]}''')
+    print(f'''Until time in ticks: {until}, mjd: {mjd_end}, datetime: {mjd2dt(mjd_end)}''')
+    smltr.power_info()
 
-# print(battery_config)
-battery = Battery(battery_config)
 
-if verbose: print(f'''Created a Battery with initial charge: {battery.level}, capacity: {battery.capacity}''')
+smltr.verbose = False # True
+smltr.simulate()
+
+# battery_config = yaml.safe_load(profile_yaml)
+
+# if vocfile!='':
+#     battery_config["VOC_table"]=vocfile
+
+# if verbose:
+#     print("Testing the battery", profile_yaml)
+#     print(f'''Battery VOC data read from the file: "{battery_config["VOC_table"]}"''')
+
+# # print(battery_config)
+# battery = Battery(battery_config)
+
+# if verbose: print(f'''Created a Battery with initial charge: {battery.level}, capacity: {battery.capacity}''')
